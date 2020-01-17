c9e97236893ff9ba31c21067ed745b66eb192c75
Rafael Lopez <lpz.raf@gmail.com>
2020-01-16 21:20:12 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./styles.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/*
+  Instructions:
+    Assume you're creating an app that allows the user to 
+    post status updates (ala Twitter). Your UI should have a
+    textarea and a button. The button should be disabled if the
+    length of the textarea is 0 or greater than 240 characters.
+    The document's title should inform the user on how many
+    characters they have left to type before they hit the 240
+    character limit - "115 characters left."
+*/
+
+function App() {
+  const [input, setInput] = React.useState('')
+
+  React.useEffect(() => {
+    document.title = `${240 - input.length} characters left.`  
+  }, [input])
+
+  return (
+    <div className="App">
+      <textarea 
+        type='text'
+        value={input}
+        placeholder='Type'
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button 
+        disabled={input.length === 0 || input.length > 240}
+        onClick={() => console.log(input)}>
+          Submit
+      </button>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
